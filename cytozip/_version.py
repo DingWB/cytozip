b29f696935892b7f43e39115b0a7d7c2bce16702
DingWB <ding.wu.bin.gm@gmail.com>
2026-04-26 20:27:52 -0700

    backup samtools pileup subprocess

--- cytozip/_version.py
+++ cytozip/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.3.0.dev15'
-__version_tuple__ = version_tuple = (0, 3, 0, 'dev15')
+__version__ = version = '0.3.0.dev16'
+__version_tuple__ = version_tuple = (0, 3, 0, 'dev16')
 
-__commit_id__ = commit_id = 'gf2078c85b'
+__commit_id__ = commit_id = 'gf0a6f13d6'
